22df7836e7fe9bc4e25fd9931aa11213791c8be1
unknown <bfml002@gmail.com>
2021-04-29 15:05:36 +0000

    all done eheh

--- src/com/lourenco/Calculator.java
+++ src/com/lourenco/Calculator.java
@@ -1,7 +1,7 @@
 package com.lourenco;
 
 public class Calculator {
-    public int add(int number1, int number2) {
+    /*public int add(int number1, int number2) {
         return number1 + number2;
     }
 
@@ -15,32 +15,25 @@ public class Calculator {
 
     public int divide(int number1, int number2){
         return number1 / number2;
-    }
+    }*/
 
-
-    /*
-
-
-     */
-
-
-    public double add1(double number1, double number2){
+    public double add(double number1, double number2) {
         return number1 + number2;
     }
 
-    public double sub1(double number1, double number2){
+    public double sub(double number1, double number2) {
         return number1 - number2;
     }
 
-    public double mul1 (double number1, int number2){
+    public double mul(double number1, double number2) {
         return number1 * number2;
     }
 
-    public double div1(int number1, int number2){
-        return number1 / number2;
-    }
-
-    public int div2(int number1, int number2){
+    public double div(double number1, double number2) {
+        if (number2 == 0) {
+            System.err.println("You cannot divide by 0!");
+            return 0;
+        }
         return number1 / number2;
     }
 }
--- src/com/lourenco/Main.java
+++ src/com/lourenco/Main.java
@@ -1,23 +1,57 @@
 package com.lourenco;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Calculator counts = new Calculator();
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter your name");
+
+        String userName = scanner.nextLine();  // Read user input
+        System.out.println(userName + " enter your 1st number: ");
+        double inputNumber1 = scanner.nextDouble();
+        System.out.println("now enter your 2nd number: ");
+        double inputNumber2 = scanner.nextDouble();
+
+
+        /*Calculator counts = new Calculator();
         int addResult = counts.add(4, 3);
         int subsResult = counts.substract(3, 3);
         int mulResult = counts.multiply(3, 4);
-        int divResult = counts.divide(10, 2);
+        int divResult = counts.divide(10, 2);*/
 
-        Calculator counts2 = new Calculator();
-        double addResult1 = counts2.add1(1.5, 1.5);
-        double subsResult1 = counts2.sub1(4.5, 1.5);
-        double mulResult1 = counts2.mul1(4.5, 2);
-        double divResult1 = counts2.div1(10, 3);
-        int divResult2 = counts2.div2(0, 5);
+        Calculator counts = new Calculator();
+        double addResult = counts.add(inputNumber1, inputNumber2);
 
-        System.out.println(addResult + "\n" + subsResult + "\n" + mulResult + "\n" + divResult + "\n");
-        System.out.println(addResult1 + "\n" + subsResult1 + "\n" + mulResult1 + "\n" + divResult1 + "\n" + divResult2 + "\n");
+        System.out.println(userName + " the sum of " + inputNumber1 + " + " + inputNumber2 + " = " + addResult + "\n");
 
+
+        System.out.println(userName + " enter your 1st number: ");
+        double inputNumber3 = scanner.nextDouble();
+        System.out.println("now enter your 2nd number: ");
+        double inputNumber4 = scanner.nextDouble();
+
+        double subsResult = counts.sub(inputNumber3, inputNumber4);
+
+        System.out.println(userName + " the subtraction of " + inputNumber3 + " - " + inputNumber4 + " = " + subsResult + "\n");
+
+        System.out.println(userName + " enter your 1st number: ");
+        double inputNumber5 = scanner.nextDouble();
+        System.out.println("now enter your 2nd number: ");
+        double inputNumber6 = scanner.nextDouble();
+
+        double mulResult = counts.mul(inputNumber5, inputNumber6);
+
+        System.out.println(userName + " the multiplication of " + inputNumber5 + " * " + inputNumber6 + " = " + mulResult + "\n");
+
+
+        System.out.println(userName + " enter your 1st number: ");
+        double inputNumber7 = scanner.nextDouble();
+        System.out.println("now enter your 2nd number: ");
+        double inputNumber8 = scanner.nextDouble();
+
+        double divResult = counts.div(inputNumber7, inputNumber8);
     }
 }
 
